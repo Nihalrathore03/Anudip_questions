a2e7b0b17826a390210fc30614711c659c6ce7a9
Nihalrathore03 <nihalrathore0207@gmail.com>
2024-11-27 10:42:30 +0000

    comments inside it........

--- Pyramid.java
+++ Pyramid.java
@@ -6,18 +6,18 @@ public class Pyramid {
         
         int n = 5;
         for (int i =1; i<=n; i++){
-
-            //System.out.println(" ");
+            //spaces
+            
 
             for(int j =1; j<=n-i; j++){
 
                 System.out.print(" ");
             }
-
+                // print the no.
             for(int k=1; k<=i ; k++){
                 System.out.print(i +" ");
             }
-            System.out.println(" ");
+            System.out.println(" "); //for new lines..
         }
     }
 }
